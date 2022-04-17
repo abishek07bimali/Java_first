60f58aa3a1550046845a2291c19f6da8b28173c6
abishek07bimali <abi0shekbimali@gmail.com>
2022-04-17 22:05:40 +0545

    Second_Day_java_project

--- src/First.java
+++ src/First.java
@@ -1,3 +1,7 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class First {
     public static void main(String[] args) {
     int maths=9;
@@ -52,5 +56,85 @@ class Volume_cuboid{
         double height=9.5;
         double Volume= length*breadth*height;
         System.out.println("The volume of cuboid is "+ Volume);
+    }
+}
+
+class  Result {
+    public static void main(String[] args) {
+        double maths=90;
+        double English=90;
+        double Science=7;
+        double Nepali=81;
+        double Sum=maths+ English+Science+Nepali;
+        double percent= Sum/4;
+        System.out.println(percent + "%");
+        String result;
+        result=(percent>70)? "First class": (percent>59)? "Upper Second class" :(percent>49)?
+                "Second class":(percent>39)?"third class" :"fail";
+        System.out.println(result);
+
+    }
+}
+
+class Name{
+    public static void main(String[] args) {
+        System.out.println("Abishek");
+        System.out.println("Bimali");
+    }
+}
+
+class Input{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String a= scanner.nextLine();
+        System.out.println(a);
+
+    }
+}
+
+
+class Sum{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the name: ");
+        String A=scanner.nextLine();
+
+        System.out.println("enter the age: ");
+        int B=scanner.nextInt();
+
+        System.out.println("enter the Address: ");
+        scanner.nextLine();
+        String c=scanner.nextLine();
+
+
+        System.out.println(A+" "+B+" "+c);
+
+//        System.out.println("my name is "+ A);
+//        System.out.println("    my Age is "+ B);
+//        System.out.println("        my Addeess is "+ c);
+    }
+
+}
+
+class Si{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the Principle: ");
+        String A=scanner.nextLine();
+
+        System.out.println("enter the Time: ");
+        String B=scanner.nextLine();
+
+        System.out.println("enter the Rate: ");
+        String C=scanner.nextLine();
+
+        int AA=Integer.parseInt(A);
+        int Ab=Integer.parseInt(B);
+        int Ac=Integer.parseInt(C);
+        float SI=(AA*Ab*Ac)/100;
+//        int Q=Integer.parseInt(SI);
+        System.out.println(SI);
+
     }
 }
